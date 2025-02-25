b9d6e4beb3fed5dfb7d3802f4c12fb3205a058aa
NagaRajaGajjala <48096528+NagaRajaGajjala@users.noreply.github.com>
2025-02-25 17:43:44 +0530

    Update hi.go

--- hi.go
+++ hi.go
@@ -1,5 +1,5 @@
 package repo
 
 func SayHi() string {
-      return string("Hello from GitHub !!!! Naga !!!! Welcome")
+      return string("Hello from GitHub !!!! Naga !!!! Welcome !!!! 1743")
   }
